f93aa39864b71c40da9195ad8908c17d1325124a
CliveWearings <70787125+CliveWearings@users.noreply.github.com>
2020-09-07 11:57:45 -0700

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,17 +1,100 @@
-@extends('_layouts.master')
+<!DOCTYPE html>
+<html>
+<title>CSCRMKB Home</title>
+<link rel="icon" 
+      type="image/png" 
+      href="pics/hanzo.png">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h5 {font-family: "Roboto", sans-serif;}
+body, html {height: 100%}
+.bgimg {
+  background-image: url('pics/homepagepic.png');
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+/* Mobile Styles */
+@media only screen and (max-width: 500px) {
+.w3-display-left {
+font-size: 125%; 
+padding-left: 20px;
+}
+}
+/* Tablet Styles */
+@media only screen and (min-width: 501px) and (max-width: 1300px) {
+ .w3-display-left {
+font-size: 150%;
+padding-left: 40px;
+}
+}
+/* Desktop Styles */
+@media only screen and (min-width: 1301px) {
+ .w3-display-left {
+font-size: 250%;
+padding-left: 70px;
+  }
+}
+p { color:#d1d1e0; 
+text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;}
 
-@section('content')
-    <h1>Welcome to Artisan Static</h1>
+a:hover { COLOR: white; }
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
+</style>
+<body>
 
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
 
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
+<div class="bgimg w3-display-container w3-text-white">
+  <div class="w3-display-left">
+    <p></br><a href="announce.html" style="text-decoration:none">ANNOUNCEMENTS</a>
+</br><a href="season1.html" style="text-decoration:none">Season 1</a>
+</br><a href="season2.html" style="text-decoration:none">Season 2</a>
+</br><a href="season3.html" style="text-decoration:none">Season 3 (COMING SOON)</a> 
+</br><a href="season4.html" style="text-decoration:none">Season 4 (COMING LESS SOON)</a></strong></p>
+  </div>
+  <div class="w3-display-topleft w3-container w3-xlarge">
+    <p><p style="color:white"> <strong>A City Stranded Cowboy's Robot Mercy Killing Buisness</p>
+  </div>
+  <div class="w3-display-bottomleft w3-container">
+    <span> 
+ <div class="w3-bar">
+	<a button class="btn sub" href="homepage3.html">Home</a>
+    <a button class="btn sub" href="index.html">Subscribe</a>
+	<a button class="btn sub" href="html.report">Report a Typo</a>
+	<a button class="btn sub" href="https://twitter.com/urgaylolhaha">Twitter</a>
+	<a button class="btn sub" href="https://www.patreon.com/urgaylol">Patreon</a>
+	<a button class="btn sub" href="gad.html">Gadgets</a></strong>
+<style>
+.btn {
+  border: none;
+  padding: 5px 5px;
+  font-size: 16px;
+  cursor: pointer;
+  display: inline-block;
+  text-decoration: none
+}
+.btn:hover {background: purple;}
+.sub {color: white;
+text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;}
+.rep {color: #b3e0ff;
 
-    <h2 id="test">Asset Compilation Test</h2>
+background-color: purple;
+}
 
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
+</style>
+    <p><sup><p style="color:#bfbfbf">&copy; the Waltp Dinsey Corporation</a></p>
+  </div>
+</div>
 
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
-@endsection
+
+
+
+</body>
+
+  
+</html>
+
+
